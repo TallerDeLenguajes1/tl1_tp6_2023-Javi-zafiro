f951caf6be1ea8133579cd9908784ad2e0485f27
Javi-zafiro <javialexiscortez2@gmail.com>
2023-05-27 22:03:56 -0300

    ejercicio 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,25 +1,52 @@
-﻿int x;
-int z;
-int r;
-double c=0;
-string y;
-Console.WriteLine("ingrese un numero");
-bool control=int.TryParse(Console.ReadLine(), out x);
-if (control && x>0)
+﻿int num1, num2;
+float resultado;
+int repetir;
+string? menu;
+bool aux=false;
+repetir=0;
+resultado=0;
+do
 {
-    y=Convert.ToString(x);
-    Console.WriteLine(y);
-    z=y.Length;
-    while (x>=10)
+    Console.WriteLine("ingrese la operacion que desea realizar");
+    Console.WriteLine("1. suma");
+    Console.WriteLine("2. resta");
+    Console.WriteLine("3. multiplicacion");
+    Console.WriteLine("4. division");
+    menu=Console.ReadLine();
+    Console.WriteLine("ingrese el primer numero");
+    int.TryParse(Console.ReadLine(), out num1);
+    Console.WriteLine("ingrese el segundo numero");
+    int.TryParse(Console.ReadLine(), out num2);
+    switch (menu)
     {
-        r=x%10;
-        c=c+(r*Math.Pow(10, z-1));
-        x=x/10;
-        z=z-1;
+        case "suma":
+            resultado=num1+num2;
+            Console.WriteLine("el resultado de la suma es: "+resultado);
+            break;
+        case "resta":
+            resultado=num1-num2;
+            Console.WriteLine("el resultado de la resta es: "+resultado);
+            break;
+        case "multiplicacion":
+            resultado=num1*num2;
+            Console.WriteLine("el resultado de la multiplicacion es: "+resultado);
+            break;
+        case "division":
+            resultado=num1/num2;
+            Console.WriteLine("el resultado de la division es: "+resultado);
+            break;
+        default:
+            Console.WriteLine("no se reconoce la palabra ingresada");
+            break;
     }
-    if (x<10)
+    Console.WriteLine("si desea realizar otra operacion ingrese 1");
+    Console.WriteLine("si desea salir precione 0");
+    int.TryParse(Console.ReadLine(), out repetir);
+    if (repetir==1)
     {
-        c=c+x;
+        aux=true;
+    }else
+    {
+        aux=false;
     }
-    Console.WriteLine(c);
-}
+} while (aux);
