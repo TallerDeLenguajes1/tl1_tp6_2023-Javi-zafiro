e79742b2f4c303e6690dc3d76b46d8a872a39bf1
Javi-zafiro <javialexiscortez2@gmail.com>
2023-05-27 22:48:37 -0300

    CALCULADORAV2 terminada, aun no se hizo el merge

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,25 +1,34 @@
-﻿int x;
-int z;
-int r;
-double c=0;
-string y;
+﻿float num, num1, num2;
 Console.WriteLine("ingrese un numero");
-bool control=int.TryParse(Console.ReadLine(), out x);
-if (control && x>0)
+float.TryParse(Console.ReadLine(), out num);
+Console.WriteLine("el numero ingresado es: "+num);
+Console.WriteLine("el valor absoluto del numero es: "+Math.Abs(num));
+Console.WriteLine("el cuadrado del numero es: "+Math.Pow(num, 2));
+if (num>=0)
 {
-    y=Convert.ToString(x);
-    Console.WriteLine(y);
-    z=y.Length;
-    while (x>=10)
+    Console.WriteLine("la raiz cuadrada del numero es: "+Math.Sqrt(num));
+}else
+{
+    Console.WriteLine("el numero es negativo no se puede calcular su raiz cuadrada");
+}
+Console.WriteLine("el seno del numero es: "+Math.Sin(num));
+Console.WriteLine("el coseno del numero es: "+Math.Cos(num));
+Console.WriteLine("la parte entera del numero es: "+Math.Floor(num));
+Console.WriteLine("vamos a comparar 2 numeros");
+Console.WriteLine("ingrese el primer numero");
+float.TryParse(Console.ReadLine(), out num1);
+Console.WriteLine("ingrese el segundo numero");
+float.TryParse(Console.ReadLine(), out num2);
+if (num1<num2)
+{
+    Console.WriteLine("el numero "+num2+" es mas grande que el numero"+num1);
+}else
+{
+    if (num1>num2)
     {
-        r=x%10;
-        c=c+(r*Math.Pow(10, z-1));
-        x=x/10;
-        z=z-1;
-    }
-    if (x<10)
+        Console.WriteLine("el numero "+num1+" es mas grande que el numero"+num2);
+    }else
     {
-        c=c+x;
+        Console.WriteLine("los numeros "+num1+" "+num2+" son iguales");
     }
-    Console.WriteLine(c);
 }
